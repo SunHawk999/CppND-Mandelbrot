73c390dec67c9987684daa190106682988d8e40f
Ethan <ethanw123@gmail.com>
2019-10-04 14:33:56 -0400

    Implement write function

--- src/BMP.h
+++ src/BMP.h
@@ -54,21 +54,39 @@ struct BMP{
     BMPColorHeader bmp_color_header;
     std::vector<uint8_t> data;
 
-/*
     //Writing an image to disk
     void write(const char *fname){
 
         std::ofstream of{fname, std::ios_base::binary};
 
-        if(of){
+        if(of)
             if(bmp_info_header.bit_count == 32)
                 write_headers_and_data(of);
-        }
+        
+        else if(bmp_info_header.bit_count == 24)
+            if(bmp_info_header.width % 4 == 0)
+                write_headers_and_data(of);
 
+            else{
+                uint32_t new_stride = make_stride_aligned(4);
+                std::vector<uint8_t> padding_row(new_stride - row_stride);
+
+                write_headers(of);
+
+                for(int y = 0; y < bmp_info_header.height; ++y){
+                    of.write((const char*)(data.data() + row_stride * y), row_stride);
+                    of.write((const char*)padding_row.data(), padding_row.size());
+                }
+            }
+        
+        else
+            throw std::runtime_error("This program can only treat 24 or 32 bits per pixel.");
+    
+    throw std::runtime_error("Unable to open the output image file.");
     }
-*/
+
 private:
-/*
+
     uint32_t row_stride{0};
 
 
@@ -86,5 +104,14 @@ private:
         write_headers(of);
         of.write((const char*)data.data(), data.size());
     }
-*/
+
+    //Add 1 to the row_stride until it is divisble with align_stride
+    uint32_t make_stride_aligned(uint32_t align_stride){
+        uint32_t new_stride = row_stride;
+        while(new_stride % align_stride != 0){
+            new_stride++;
+        }
+
+        return new_stride;
+    }
 };
