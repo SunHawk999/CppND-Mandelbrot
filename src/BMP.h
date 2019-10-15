9d4fdef5856e08cfe270afc3fcdc12548e41095e
Ethan <ethanw123@gmail.com>
2019-10-15 14:21:10 -0400

    Clean and organize code

--- src/BMP.h
+++ src/BMP.h
@@ -1,9 +1,12 @@
+#ifndef BMP_h
+#define BMP_h
 
 #pragma once
 #include<fstream>
 #include<stdexcept>
 #include<vector>
 #include<iostream>
+#include<complex>
 
 //Header code based from https://solarianprogrammer.com/2018/11/19/cpp-reading-writing-bmp-images/
 
@@ -46,136 +49,39 @@ struct BMPColorHeader{
 };
 #pragma pack(pop)
 
+struct BMP {
 
-struct BMP{
-
+public:
     BMPFileHeader file_header;
     BMPInfoHeader bmp_info_header;
     BMPColorHeader bmp_color_header;
     std::vector<uint8_t> data;
 
     //Create a BMP image in memory
-    BMP(int32_t width, int32_t height, bool has_alpha = true){
-
-        if(width <= 0 || height <= 0)
-            throw std::runtime_error("The image width and height must be positive numbers.");
-
-        bmp_info_header.width = width;
-        bmp_info_header.height = height;
-
-        if(has_alpha){
-            bmp_info_header.size = sizeof(BMPInfoHeader) + sizeof(BMPColorHeader);
-            file_header.offset_data = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader) + sizeof(BMPColorHeader);
-
-            bmp_info_header.bit_count = 32;
-            bmp_info_header.compression = 3;
-            row_stride = width * 4;
-            data.resize(row_stride * height);
-            file_header.file_size = file_header.offset_data + data.size();
-        }
-        else{
-            bmp_info_header.size = sizeof(BMPInfoHeader);
-            file_header.offset_data = sizeof(BMPFileHeader) + sizeof(BMPInfoHeader);
-
-            bmp_info_header.bit_count = 24;
-            bmp_info_header.compression = 0;
-            row_stride = width * 3;
-            data.resize(row_stride * height);
-
-            uint32_t new_stride = make_stride_aligned(4);
-            file_header.file_size = file_header.offset_data + data.size() + bmp_info_header.height * (new_stride - row_stride);
-        }
-    }
+    BMP(int32_t width, int32_t height, bool has_alpha = true);
 
     //Modify a specified region of a created or read bmp image
-    void fillRegion(uint32_t x0, uint32_t y0, uint32_t w, uint32_t h, uint8_t R, uint8_t G, uint8_t B, uint8_t A){
-
-        if(x0 + w > (uint32_t)bmp_info_header.width || y0 + h > (uint32_t)bmp_info_header.height)
-            throw std::runtime_error("This region does not fit in the image!");
-
-        uint32_t channels = bmp_info_header.bit_count / 8;
-        for(uint32_t x = x0; x < x0 + w; ++x){
-            for(uint32_t y = y0; y < y0 + h; ++y){
-                data[channels * (y * bmp_info_header.width + x) + 2] = R;
-                data[channels * (y * bmp_info_header.width + x) + 1] = G;
-                data[channels * (y * bmp_info_header.width + x) + 0] = B;
-
-                if(channels == 4)
-                    data[channels * (y * bmp_info_header.width + x) + 3] = A;
-            }
-        }
-    }
+    void fillRegion(uint32_t x0, uint32_t y0, uint32_t w, uint32_t h, uint8_t R, uint8_t G, uint8_t B, uint8_t A);
 
     //Writing an image to disk
-    void write(const char *fname){
+    void write(const char *fname);
 
-        std::ofstream of{fname, std::ios_base::binary};
+    //Given a the coordinates of a point, check if the convergences for c = x + i*y, and returns
+    //a value, 255 or 0
+    void mandelbrot(uint32_t x, uint32_t y, uint8_t R, uint8_t B, uint8_t G, uint8_t A);
 
-        if(of){
-            if(bmp_info_header.bit_count == 32)
-                write_headers_and_data(of);
-
-            else if(bmp_info_header.bit_count == 24){
-                if(bmp_info_header.width % 4 == 0)
-                    write_headers_and_data(of);
-
-                else{
-                    uint32_t new_stride = make_stride_aligned(4);
-                    std::vector<uint8_t> padding_row(new_stride - row_stride);
-
-                    write_headers(of);
-
-                    for(int y = 0; y < bmp_info_header.height; ++y){
-                        of.write((const char*)(data.data() + row_stride * y), row_stride);
-                        of.write((const char*)padding_row.data(), padding_row.size());
-                    }
-                }
-            }
-            else
-                throw std::runtime_error("This program can only treat 24 or 32 bits per pixel.");
-        }
-        else
-            throw std::runtime_error("Unable to open the output image file.");
-    }
 
 private:
-
     uint32_t row_stride{0};
 
-    void write_headers(std::ofstream &of){
+    void write_headers(std::ofstream &of);
 
-        of.write((const char*)&file_header, sizeof(file_header));
-        of.write((const char*)&bmp_info_header, sizeof(bmp_info_header));
-
-        if(bmp_info_header.bit_count = 32)
-            of.write((const char*)&bmp_color_header, sizeof(bmp_color_header));
-    }
-
-    void write_headers_and_data(std::ofstream &of){
-        write_headers(of);
-        of.write((const char*)data.data(), data.size());
-    }
+    void write_headers_and_data(std::ofstream &of);
 
     //Add 1 to the row_stride until it is divisble with align_stride
-    uint32_t make_stride_aligned(uint32_t align_stride){
-        uint32_t new_stride = row_stride;
-        while(new_stride % align_stride != 0){
-            new_stride++;
-        }
-        return new_stride;
-    }
+    uint32_t make_stride_aligned(uint32_t align_stride);
 
     // Check if the pixel data is stored as BGRA and if the color space type is sRGB
-    void check_color_header(BMPColorHeader &bmp_color_header) {
-        BMPColorHeader expected_color_header;
-        if(expected_color_header.red_mask != bmp_color_header.red_mask ||
-            expected_color_header.blue_mask != bmp_color_header.blue_mask ||
-            expected_color_header.green_mask != bmp_color_header.green_mask ||
-            expected_color_header.alpha_mask != bmp_color_header.alpha_mask) {
-            throw std::runtime_error("Unexpected color mask format! The program expects the pixel data to be in the BGRA format");
-        }
-        if(expected_color_header.color_space_type != bmp_color_header.color_space_type) {
-            throw std::runtime_error("Unexpected color space type! The program expects sRGB values");
-        }
-    }
+    void check_color_header(BMPColorHeader &bmp_color_header);
 };
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "BMP.h"
+//#include "Drawing.h"
+#include<iostream>
 
 //Start by creating an image making program. Be sure to refrence where the code was based from.
 
@@ -6,9 +8,13 @@
 int main(){
 
     //Create a BMP image in memory, modify it and save to disk
-    BMP bmp1(800, 600);
-    bmp1.fillRegion(50, 20, 100, 200, 0, 0, 255, 255);
-    bmp1.write("image_test.bmp");
+    BMP bmp1(800, 800);
 
+    //bmp1.fillRegion(0, 0, 800, 800, 255, 255, 255, 0);
+    
+   // bmp1.mandelbrot(800, 800, 0, 150, 0, 0);
+    
+    bmp1.write("image_test.bmp");
+ 
     return 0;
 }
