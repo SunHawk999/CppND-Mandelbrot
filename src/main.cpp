e892b022bdea7b70a398386a5c28d94bfe141f56
Ethan <ethanw123@gmail.com>
2019-10-15 15:35:19 -0400

    Clean up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "BMP.h"
-//#include "Drawing.h"
 #include<iostream>
 
 //Start by creating an image making program. Be sure to refrence where the code was based from.
@@ -10,9 +9,9 @@ int main(){
     //Create a BMP image in memory, modify it and save to disk
     BMP bmp1(800, 800);
 
-    //bmp1.fillRegion(0, 0, 800, 800, 255, 255, 255, 0);
+    bmp1.fillRegion(0, 0, 800, 800, 100, 200, 150, 0);
     
-   // bmp1.mandelbrot(800, 800, 0, 150, 0, 0);
+    //bmp1.mandelbrot(800, 800, 0, 150, 0, 100);
     
     bmp1.write("image_test.bmp");
  
