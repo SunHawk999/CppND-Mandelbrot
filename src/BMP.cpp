6688d57b880b07fadad0818befc2193cb275a7d3
Ethan <ethanw123@gmail.com>
2019-10-15 15:32:11 -0400

    Troubleshoot mandelbrot function

--- src/BMP.cpp
+++ src/BMP.cpp
@@ -93,13 +93,6 @@ void BMP::fillRegion(uint32_t x0, uint32_t y0, uint32_t w, uint32_t h, uint8_t R
 //Given a the coordinates of a point, check if the convergences for c = x + i*y, and returns
 //a value, 255 or 0
 void BMP::mandelbrot(uint32_t x, uint32_t y, uint8_t R, uint8_t B, uint8_t G, uint8_t A){
-
-    //Divide by the image dimensions to get values smaller than 1, then apply a translation
-    std::complex<float> point( (float)x/bmp_info_header.width - 1.5, (float)y/bmp_info_header.height - 0.5);
-
-    //translation
-    std::complex<float> z(0, 0);
-
         //Set a point on the image in memory to 255 or 0 based on mandelbrot
         //Need to use channels somehow fro BMP.h
         /*
@@ -110,6 +103,12 @@ void BMP::mandelbrot(uint32_t x, uint32_t y, uint8_t R, uint8_t B, uint8_t G, ui
 
         for(uint32_t i = 0; i < bmp_info_header.width; ++i){
             for(uint32_t j = 0; j < bmp_info_header.height; ++j){
+
+                //Divide by the image dimensions to get values smaller than 1, then apply a translation
+                std::complex<float> point( (float)x/bmp_info_header.width - 1.5, (float)y/bmp_info_header.height - 0.5);
+
+                //translation
+                std::complex<float> z(0, 0);
 
                 unsigned int nb_iter = 0;
                 while(abs(z) < 2 && nb_iter <= 34){
